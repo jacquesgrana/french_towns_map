873283d009cd5ac62849fc516ac28bfb8fb984c9
jacques <jac.granarolo.info@gmail.com>
2024-07-27 14:19:25 +0200

    add map in homepage + functions in TownRepository

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\TownRepository;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(TownRepository $townRepository): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        $townsGps = $townRepository->findByBoundingBox(43.520769, 3.752716, 43.700769, 4.000716);
+
+        $townsName = $townRepository->findByName('Montp');
+
+        return $this->render('home/index.html.twig', 
+        [
+            'townsGps' => $townsGps,
+            'townsName' => $townsName
         ]);
     }
 
--- src/Repository/TownRepository.php
+++ src/Repository/TownRepository.php
@@ -5,15 +5,21 @@ namespace App\Repository;
 use App\Entity\Town;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManagerInterface;
 
 /**
  * @extends ServiceEntityRepository<Town>
  */
 class TownRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
+    private $em;
+    public function __construct(
+        ManagerRegistry $registry,
+        EntityManagerInterface $em
+        )
     {
         parent::__construct($registry, Town::class);
+        $this->em = $em;
     }
 
     //    /**
@@ -40,4 +46,59 @@ class TownRepository extends ServiceEntityRepository
     //            ->getOneOrNullResult()
     //        ;
     //    }
+
+    public function findByBoundingBox(float $swLat, float $swLng, float $neLat, float $neLng): array
+    {
+        $query = $this->em->createQuery(
+            'SELECT 
+            t.id, 
+            t.townCode, 
+            t.townZipCode, 
+            t.townName, 
+            d.depName, 
+            r.regName, 
+            p.latitude, 
+            p.longitude 
+            FROM App\Entity\Town t
+            JOIN t.positionGps p
+            JOIN t.departement d
+            JOIN d.region r
+            WHERE p.latitude >= :swLat 
+            AND p.longitude >= :swLng
+            AND p.latitude <= :neLat 
+            AND p.longitude <= :neLng'
+        );
+
+        $query->setParameters([
+            'swLat' => $swLat,
+            'swLng' => $swLng,
+            'neLat' => $neLat,
+            'neLng' => $neLng,
+        ]);
+        return $query->getResult();
+    }
+
+    public function findByName(string $nameToFind): array
+    {
+        $query = $this->em->createQuery(
+            'SELECT 
+            t.id, 
+            t.townCode, 
+            t.townZipCode, 
+            t.townName, 
+            d.depName, 
+            r.regName, 
+            p.latitude, 
+            p.longitude 
+            FROM App\Entity\Town t
+            JOIN t.positionGps p
+            JOIN t.departement d
+            JOIN d.region r
+            WHERE UPPER(t.townName) LIKE :nameToFind'
+        );
+    
+        $query->setParameter('nameToFind', '%' . $nameToFind . '%');
+
+        return $query->getResult();
+    }
 }
