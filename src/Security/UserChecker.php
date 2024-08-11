bb0f9a4af2257b90201b4d2273501f6796b1025d
jacques <jac.granarolo.info@gmail.com>
2024-08-11 14:42:11 +0200

    add modify user account password feature

--- src/Security/UserChecker.php
+++ src/Security/UserChecker.php
@@ -4,14 +4,14 @@ namespace App\Security;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Core\User\UserCheckerInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
-//use App\Service\MailerService;
+use App\Service\MailerService;
 
 
 class UserChecker implements UserCheckerInterface
 {
 
     public function __construct(
-        //private MailerService $mailer
+        private MailerService $mailer
     )
     {}
 
@@ -19,8 +19,8 @@ class UserChecker implements UserCheckerInterface
     {
         /** @var \App\Entity\User $user **/
         if (!$user->isActive()) {
-            //$this->mailer->GenerateAnsSendConfirmSigninEmail($user);
-            throw new CustomUserMessageAuthenticationException('Votre compte n\'est pas actif. Un email de confirmation vous a été envoyé. Veuillez le confirmer.');
+            $this->mailer->GenerateAndSendConfirmRegisterEmail($user);
+            throw new CustomUserMessageAuthenticationException('Votre compte n\'est pas actif. Un email de confirmation vous a été envoyé. Veuillez cliquer sur le lien contenu par l\'email pour activer votre compte.');
         }
     }
 
