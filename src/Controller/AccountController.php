0b99c36073f89e99cc6977561ad18338ff1e4cd4
jacques <jac.granarolo.info@gmail.com>
2024-08-11 13:30:20 +0200

    add update account email feature with activation mail sending system

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -10,6 +10,9 @@ use Symfony\Component\HttpFoundation\Response;
 use App\Form\UserEditAccountType;
 use App\Entity\User;
 use App\Repository\UserRepository;
+use App\Service\MailerService;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Validation;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 #[Route('/user/account')]
@@ -73,4 +76,70 @@ class AccountController extends AbstractController
         ]);
     }
 
+    #[Route('/edit/email', name: 'app_user_account_edit_email', methods: ['GET', 'POST'])]
+    public function modifyEmail (
+        ): Response
+    {
+        /** @var User $user **/
+        $user = $this->getUser();
+        // recuperer l'email a partir de la query string
+        $email = $user->getEmail();
+        return $this->render('account/account.edit.email.html.twig', [
+            'email' => $email
+        ]);
+    }
+
+    #[Route('/update/email', name: 'app_user_account_update_email', methods: ['GET', 'POST'])]
+    public function setEmail (
+        Request $request, 
+        EntityManagerInterface $entityManager,
+        UserRepository $userRepository,
+        MailerService $mailerService
+        ): Response
+    {
+        /** @var \App\Entity\User $user **/
+        $user = $this->getUser();
+        $oldEmail = $user->getEmail();
+        $email = $request->request->get('email');
+        $csrf_token = $request->request->get('_csrf_token');
+        $validator = Validation::createValidator();
+        $violations = $validator->validate($email, new Email());
+        $isUniqueEmail = $userRepository->findOneByEmail($email) === null;
+        $isEmailValid = 0 === count($violations);
+        $isTokenValid = $this->isCsrfTokenValid('change-email', $csrf_token);
+        $isEmailsNotTheSame = $email != $oldEmail;
+
+        if (!$isUniqueEmail || !$isEmailValid || !$isTokenValid || !$isEmailsNotTheSame) {
+            if(!$isUniqueEmail) {
+                $this->addFlash('error', 'Email non unique');
+            }
+            if(!$isEmailValid) {
+                $this->addFlash('error', 'Email invalide');
+            }
+            if(!$isTokenValid) {
+                $this->addFlash('error', 'Token invalide');
+            }
+            if(!$isEmailsNotTheSame) {
+                $this->addFlash('error', 'Emails identiques');
+            }
+            return $this->redirectToRoute('app_user_account_edit_email', [], Response::HTTP_SEE_OTHER);
+        }
+        else {
+            // TODO : améliorer : tenir compte du cas ou le nouvel email est fonctionnel ?
+            $user->setEmail($email);
+            $user->setActive(false);
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $result = $mailerService->GenerateAndSendConfirmRegisterEmail($user);
+            //GenerateAnsSendConfirmSigninEmail
+            if ($result === 'Email ok') {
+                $this->addFlash('success', 'Email d\'activation envoyé');
+            }
+            else {
+                $this->addFlash('error', $result);
+            }
+            return $this->redirectToRoute('app_logout', [], Response::HTTP_SEE_OTHER);
+        }
+    }
 }
